3982913ecb3d2776b96d861cf234d08a0ced04d3
Mona Lisa <patrinoua@gmail.com>
2018-07-23 22:38:21 +0200

    first commit

---
+++ webpageContent.js
@@ -0,0 +1,33 @@
+
+exports.contentEn={
+    name:'Jannis Polyzos',
+
+    about:'About',
+    work:'Work',
+    contact:'Contact',
+
+    title:'Massage Therapist',
+
+    personalDescription:`
+        I am a Massage Therapist living in Berlin.
+        I have studied Theater and Dance,
+        I like giving massages
+        `,
+
+    myFont:'Quicksand'
+}
+exports.contentGr={
+    name:'Γιάννης Πολύζος',
+
+    about:'Σχετικά',
+    work:'Πορτφόλιο',
+    contact:'Επικοινωνία',
+
+    title:'Θεραπευτής Μασάζ',
+
+    personalDescription:`
+        Με λένε Γιάννη Πολύζο και ζω στο Βερολίνο.
+        Έχω σπουδάσει Θέατρο και Χορό και μου αρέσει να κάνω μασαζζζ.
+        `,
+    myFont:'Helvetica'
+}
