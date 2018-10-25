f46702a51e14e3360d1eb40707d6ea8c5e710f7f
Angeliki Patrinou <angeliki.patrinou@modifi.com>
2018-10-25 16:29:54 +0200

    update description

--- webpageContent.js
+++ webpageContent.js
@@ -9,9 +9,11 @@ exports.contentEn={
     title:'Massage Therapist',
 
     personalDescription:`
-        I am a Massage Therapist living in Berlin.
-        I have studied Theater and Dance,
-        I like giving massages
+    My love for massage started in rehearsal. As a dancer and an actor, after hours of
+practicing I would then give my fellow dancers a much sought a full body massage
+to help them relax. A while ago I decided to take my relationship with massage to
+the next level and undertake lessons. I am now a certified Thai massage therapist,
+looking to take people's pain off their backs!
         `,
 
     myFont:'Quicksand'
@@ -26,8 +28,8 @@ exports.contentGr={
     title:'Θεραπευτής Μασάζ',
 
     personalDescription:`
-        Με λένε Γιάννη Πολύζο και ζω στο Βερολίνο.
-        Έχω σπουδάσει Θέατρο και Χορό και μου αρέσει να κάνω μασαζζζ.
+    Ονομάζομαι Γιάννης Πολύζος απο Αθήνα και με έδρα το Βερολίνο όπου τα τελευταία χρόνια ζώ και εργάζομαι. Με σπουδές στο Θέατρο και τον Χορό απ’όπου η πολυετής εμπειρία μου ως χορευτής με έφερε κοντά σε αυτή τη θαυμάσια μηχανή που λέγεται σώμα. Το ενδιαφέρον μου για το σώμα επικεντρώνεται σε ότι αφορά τις κινητικές ικανότητες που έχει το ίδιο μέσω του χορού, μέσω της κίνησης γενικότερα, αλλά και τις θεραπαυτικές ικανότητες που έχει ο ίδιος ο οργανισμός, όπως επίσης και για το πως μπορούμε να θεραπεύσουμε-ξεκουράσουμε-τονώσουμε ενα σώμα μέσω της μάλαξης.
+      Η επαφή μου με την μάλαξη ξεκινά αρκετά χρόνια πρίν εντελώς ερασιτεχνικά στην αρχή, θέλοντας να ξεκουράσω συναδέλφους και φίλους χορευτές, ανάγκη που είχα κι εγω βεβαίως ο ίδιος. Κατά τη διάρκεια παραμονής μου στο Βερολίνο μου δόθηκε η ευκαιρία να σπουδάσω την Ταυλανδέζικη μάλαξη και να εμπλουτίσω τις γνώσεις μου πάνω σε βασικές έννοιες όπως Σώμα, Πόνος, Ανακούφιση, Θεραπεία!
         `,
     myFont:'Helvetica'
 }
