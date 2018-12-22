873ffbbee4ea2b190ea78edbf3d2d96f6a441042
Mona Lisa <patrinoua@gmail.com>
2018-12-23 00:23:47 +0200

    projects -> services

--- index.js
+++ index.js
@@ -4,54 +4,43 @@ const bodyParser = require('body-parser');
 const hb = require('express-handlebars');
 const fs = require('fs');
 
-const {contentEn, contentGr} =require('./webpageContent.js');
 
 app.engine('handlebars',hb());
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname+'/public'));
-app.use(express.static(__dirname+'/projects'));
+app.use(express.static(__dirname+'/services'));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-let myProjects = fs.readdirSync(__dirname+'/projects');
+const {contentEn, contentGr} =require('./webpageContent.js');
 
-var myProjectObjects = myProjects.map(function(project){
+let servicesList = fs.readdirSync(__dirname+'/services');
+
+var services = servicesList.map(function(service){
     return {
-        dirName: project,
-        heroku_url: require('./projects/' + project + '/project.json' ).heroku_url,
-        github_url: require('./projects/' + project + '/project.json' ).github_url,
-        createdWith: require('./projects/' + project + '/project.json' ).createdWith,
-        comment_link: require('./projects/' + project + '/project.json' ).comment_link,
-        comment_url: require('./projects/' + project + '/project.json' ).comment_url,
-        displayName: require('./projects/' + project + '/project.json' ).displayName,
-        description: require('./projects/' + project + '/project.json' ).description,
-        tryMsg: require('./projects/' + project + '/project.json' ).tryMsg,
-        codeMsg: require('./projects/' + project + '/project.json' ).codeMsg,
-        comment: require('./projects/' + project + '/project.json' ).comment
+        dirName: service,
+        displayName: require('./services/' + service + '/project.json' ).displayName,
+        description: require('./services/' + service + '/project.json' ).description,
+        tryMsg: require('./services/' + service + '/project.json' ).tryMsg,
+        comment: require('./services/' + service + '/project.json' ).comment
     }
 });
-var myProjectObjectsGr = myProjects.map(function(project){
-    return {
-        dirName: project,
-        heroku_url: require('./projects/' + project + '/project.json' ).heroku_url,
-        github_url: require('./projects/' + project + '/project.json' ).github_url,
-        comment_link: require('./projects/' + project + '/project.json' ).comment_link,
-        comment_url: require('./projects/' + project + '/project.json' ).comment_url,
 
-        displayName: require('./projects/' + project + '/project.json' ).displayNameGr ||"displaynamestaellliiinikkaaaa",
-        description: require('./projects/' + project + '/project.json' ).descriptionGr,
-        tryMsg: require('./projects/' + project + '/project.json' ).tryMsgGr,
-        codeMsg: require('./projects/' + project + '/project.json' ).codeMsgGr,
-        createdWith: require('./projects/' + project + '/project.json' ).createdWithGr,
-        comment:require('./projects/' + project + '/project.json' ).commentGr
+var servicesGr = servicesList.map(function(service){
+    return {
+        dirName: service,
+        displayName: require('./services/' + service + '/project.json' ).displayNameGr ||"displaynamestaellliiinikkaaaa",
+        description: require('./services/' + service + '/project.json' ).descriptionGr,
+        tryMsg: require('./services/' + service + '/project.json' ).tryMsgGr,
+        comment:require('./services/' + service + '/project.json' ).commentGr
     }
 });
 
 app.get('/',function(req,res){
     res.render('projectData',{
         content:contentEn,
-        myProjectObjects:myProjectObjects,
+        services:services,
         layout:'welcome'
     })
 })
@@ -59,7 +48,7 @@ app.get('/',function(req,res){
 app.get('/gr',function(req,res){
     res.render('projectData',{
         content:contentGr,
-        myProjectObjects:myProjectObjectsGr,
+        services:servicesGr,
         // projectsText:projectsTextGr,
         layout:'welcome'
     })
